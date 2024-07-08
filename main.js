085b4f0700dad1920646916984a1379f2d6ae6a9
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-07-08 23:18:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//Create a function that takes the age in years and returns the age in days.
+
+// years >>> days
+//years * 365
+
+function calcAge(age) {
+    return age * 365;
+}
+console.log(calcAge(65));
+console.log(calcAge(0));
+console.log(calcAge(20));
